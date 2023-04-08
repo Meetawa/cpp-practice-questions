0896ae9c8f816dae7b2dba8dcaae32d37662f2f4
RAHUL <rahulmeetawa@gmail.com>
2023-04-08 12:59:06 +0530

    add two number

---
+++ add_two_number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int var1, var2;
+    cout << "enter the values which you add :" << endl;
+    cin >> var1 >> var2;
+
+    int sum;
+    sum = var1 + var2;
+    cout << "sum is :" << endl
+         << sum;
+}
