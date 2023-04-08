8ec2622635f08168d827ced6872ad2f5debacab6
RAHUL <rahulmeetawa@gmail.com>
2023-04-08 13:15:04 +0530

    MUL

---
+++ Multiply_Numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cout << "enter the value of x and y :" << endl;
+    cin >> x >> y;
+    int mul;
+    mul = x * y;
+    cout << " mult is :" << endl
+         << mul;
+    return 0;
+}
